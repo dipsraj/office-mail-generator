daa13291020829ec50d31ece5f145d1194e3e16c
dipanjan <dipsraj.kundu@gmail.com>
2020-02-18 23:54:37 +0530

    Initial Commit

---
+++ precache-manifest.f7998a8bd4ac4d53a7f6a381ef03d8f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84db167a58096debbbf1524bb1abed2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e28a495374cca5d62ce4",
+    "url": "/static/css/main.dbc5eebb.chunk.css"
+  },
+  {
+    "revision": "d7e76aa20015c424cdae",
+    "url": "/static/js/2.5abc50c0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.5abc50c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28a495374cca5d62ce4",
+    "url": "/static/js/main.34f5f75c.chunk.js"
+  },
+  {
+    "revision": "c85a2bc4289c7c807d17",
+    "url": "/static/js/runtime-main.48397105.js"
+  }
+]);
